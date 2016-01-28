fa8bfac9b06df9857cda67dd4dae358bd3f42515
A.I <ailis@paw.zone>
2016-01-28 19:22:19 +0900

    Refactoring coding style

--- build.sh
+++ build.sh
@@ -6,16 +6,17 @@ TAG=ailispaw/ubuntu-essential
 
 set -ve
 
-docker build -t ubuntu-essential-multilayer - <<'EOF'
+docker build -t ubuntu-essential-multilayer - <<EOF
 FROM ubuntu:14.04
 # Make an exception for apt: it gets deselected, even though it probably shouldn't.
 RUN dpkg --clear-selections && echo "apt install" | dpkg --set-selections && \
     SUDO_FORCE_REMOVE=yes DEBIAN_FRONTEND=noninteractive apt-get --purge -y dselect-upgrade && \
-    dpkg-query -Wf '${db:Status-Abbrev}\t${binary:Package}\n' | grep '^.i' | awk -F'\t' '{print $2 " install"}' | dpkg --set-selections && \
+    dpkg-query -Wf '${db:Status-Abbrev}\t${binary:Package}\n' | \
+      grep '^.i' | awk -F'\t' '{print $2 " install"}' | dpkg --set-selections && \
     rm -r /var/cache/apt /var/lib/apt/lists
 EOF
 
-TMP_FILE="`mktemp -t ubuntu-essential-XXXXXX`.tar.gz"
+TMP_FILE="$(mktemp -t ubuntu-essential-XXXXXX).tar.gz"
 
 docker run --rm -i ubuntu-essential-multilayer tar zpc --exclude=/etc/hostname \
   --exclude=/etc/resolv.conf --exclude=/etc/hosts --one-file-system / > "$TMP_FILE"
