952da9900b55a6dcabe1597a8762f71ead0f513f
A.I <ailis@paw.zone>
2017-05-05 12:42:37 -0700

    Update the base ubuntu image
    - 16.04, xenial-20170505
    - 14.04, trusty-20170505

--- trusty.sh
+++ trusty.sh
@@ -3,7 +3,7 @@
 TAG=ailispaw/ubuntu-essential
 VERSION=14.04
 CODENAME=trusty
-REVISION=20170330
+REVISION=20170505
 
 set -ve
 
--- xenial.sh
+++ xenial.sh
@@ -3,7 +3,7 @@
 TAG=ailispaw/ubuntu-essential
 VERSION=16.04
 CODENAME=xenial
-REVISION=20170428
+REVISION=20170505
 
 set -ve
 
