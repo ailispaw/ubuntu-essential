f7b87cb5215533d4fa76973b2c9c00a7cbc5cbea
A.I <ailis@paw.zone>
2019-10-11 07:07:21 -0700

    Update the base ubuntu image
    - 18.04, bionic-20191010
    - 16.04, xenial-20191010

--- bionic.sh
+++ bionic.sh
@@ -3,7 +3,7 @@
 TAG=ailispaw/ubuntu-essential
 VERSION=18.04
 CODENAME=bionic
-REVISION=20190930
+REVISION=20191010
 
 set -ve
 
--- xenial.sh
+++ xenial.sh
@@ -3,7 +3,7 @@
 TAG=ailispaw/ubuntu-essential
 VERSION=16.04
 CODENAME=xenial
-REVISION=20191002
+REVISION=20191010
 
 set -ve
 
